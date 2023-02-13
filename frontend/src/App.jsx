943e96c7afc865dce79ec3b20fc6bbf5e1d31884
Fannci <donot322bm@gmail.com>
2023-02-13 10:26:49 -0500

    feature initial changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,15 @@
-import { useState } from 'react'
-import Home from '../components/home'
-import './App.css'
+import { useState } from "react";
+import Home from "./components/home";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
+    <div className='App'>
       <div className='App-header'>
-
-      <Home /> 
-      
+        <Home />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/home.module.css";
+
+const Home = () => {
+  return <div>{`Empezando con nuestro proyecto <3 `}</div>;
+};
+
+export default Home;
