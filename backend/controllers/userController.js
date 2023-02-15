5fedadb6568eea94f08243842c43696610ef7ba0
CristianVelarde98 <cristianvelardefx@gmail.com>
2023-02-15 17:23:58 -0300

    modelos

---
+++ backend/controllers/userController.js
@@ -0,0 +1,1 @@
+console.log('ramiro manda')
--- backend/routes/index.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require("express");
-const router = Router();
-const users = require("./user.route");
-
-router.use("/users", users);
-
-module.exports = router;
