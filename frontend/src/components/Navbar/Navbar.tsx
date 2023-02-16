bd96b1eda558ec8c249c35da98e949362009a27e
Duvanrozop <rozoparraduvan@gmail.com>
2023-02-16 03:39:01 -0500

    add new component navbar

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,20 @@
-import React from "react";
-
-const Navbar = () => {
-  return <div>Navbar</div>;
-};
-
-export default Navbar;
+import './Navbar.css';
+import icon from '../../assets/icons/logo_icon.png';
+import iconSign from '../../assets/icons/signin-up_icon.png';
+export default function Navbar() {
+  const options = ['mentores', 'buckets'];
+  return (
+    <nav>
+      <img id='logo' src={icon} alt='logo Learnify' />
+      <div className='containerOptions'>
+        {options.map((option: string) => (
+          <div className='options'>{option}</div>
+        ))}
+      </div>
+      <div className='sign'>
+        <img src={iconSign} alt='icon Sing' />
+        Sign In/Up
+      </div>
+    </nav>
+  );
+}
