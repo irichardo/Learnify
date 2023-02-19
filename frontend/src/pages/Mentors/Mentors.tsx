d5f361f29e164ffd70392a73d0ccb1814222f435
Duvanrozop <rozoparraduvan@gmail.com>
2023-02-19 14:16:29 -0500

    implement detail for mentors

--- frontend/src/pages/Mentors/Mentors.tsx
+++ frontend/src/pages/Mentors/Mentors.tsx
@@ -1,19 +1,12 @@
-import React from 'react';
-import CardMentors from '../../components/CardMentors/CardMentors';
-import { FiltersMentors } from '../../components/FiltersMentors/FiltersMentors';
-import ShowCard from '../../components/ShowCard/ShowCard';
-import style from '../styles/Mentors.module.css';
+import './Mentors.css';
+import DetailMentor from '../../components/DetailMentor/DetailMentor';
+
 
 const Mentors = () => {
   return (
-    <div className='container m-auto columns-2 bg-red-500 justify-center'>
-      <div className='h-screen bg-blue-900'>{<ShowCard />}</div>
-      <div className='w-92 h-screen inline'>
-        <FiltersMentors />
-
-        <CardMentors />
-      </div>
-    </div>
+    <section className='containerMentors'>
+      <DetailMentor />
+    </section>
   );
 };
 
