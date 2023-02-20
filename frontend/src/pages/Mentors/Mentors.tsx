c03ce803e477790278349fda4345b9e1ef4f56f6
Duvanrozop <rozoparraduvan@gmail.com>
2023-02-20 00:29:29 -0500

    adding new component

--- frontend/src/pages/Mentors/Mentors.tsx
+++ frontend/src/pages/Mentors/Mentors.tsx
@@ -1,11 +1,12 @@
 import './Mentors.css';
 import DetailMentor from '../../components/DetailMentor/DetailMentor';
-
+import CardsListMentors from '../../components/CardsListMentors/CardsListMentors';
 
 const Mentors = () => {
   return (
     <section className='containerMentors'>
       <DetailMentor />
+      <CardsListMentors />
     </section>
   );
 };
