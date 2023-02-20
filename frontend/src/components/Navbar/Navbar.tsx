0a0740dd33ff8fac9faf6150fee2b61e9aa1e19e
Duvanrozop <rozoparraduvan@gmail.com>
2023-02-20 10:15:27 -0500

    logic implement

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,18 +1,20 @@
-import { Link } from 'react-router-dom';
 import './Navbar.css';
 import icon from '../../assets/icons/logo_icon.png';
 import iconSign from '../../assets/icons/signin-up_icon.png';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
+
 export default function Navbar() {
-  const options = ['mentores', 'buckets'];
+  const options = ['mentors', 'buckets'];
   return (
     <>
       <nav className='containerNavbar'>
-        <img id='logo' src={icon} alt='logo Learnify' />
+        <Link to='/'>
+          <img id='logo' src={icon} alt='logo Learnify' />
+        </Link>
         <div className='containerOptions'>
           {options.map((option: string, index: number) => (
-            <div key={index * 21} className='options'>
-              {option}
+            <div key={option} className='options'>
+              <Link to={`/${option}`}>{option}</Link>
             </div>
           ))}
         </div>
@@ -23,7 +25,7 @@ export default function Navbar() {
       </nav>
 
       <Outlet />
-      {/* ^ aca le digo donde debe renderizar los hijos  */}
+      {/* ^^^^^ aca le digo donde debe renderizar los hijos  */}
     </>
   );
 }
