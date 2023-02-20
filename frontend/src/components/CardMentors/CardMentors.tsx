312b4072501392b271506e8faae6d760f40ecfc9
Duvanrozop <rozoparraduvan@gmail.com>
2023-02-20 00:26:58 -0500

    cards the of other teacher with his styles

--- frontend/src/components/CardMentors/CardMentors.tsx
+++ frontend/src/components/CardMentors/CardMentors.tsx
@@ -1,35 +1,35 @@
-import React from 'react';
-import style from './CardMentors.module.css';
-import data from '../ShowCard/data.json';
-// import Language from '../ShowCard/Language';
+import './CardMentors.css';
 
-const CardMentors = () => {
-  // let aux = Language.map((lan) => {});
-  // console.log(Language);
-  // let icon = Language.map((ico) => ico.icon);}
-  // console.log(icon, 'icon');
-  console.log(data.map((dat) => dat.language));
+import imgPredeterminate from '../../assets/imgsPrueba/icon_perfil2.png';
+
+import { ValidateMentors, Cards } from '../../helpers/Types/Cards';
+
+interface PropsCardsMentors {
+  ArrayMentors: ValidateMentors;
+}
+
+function CardMentor({ img, cargo, nombre }: Cards) {
   return (
-    <div className={style.cards}>
-      {data.map((mentor) => {
-        return (
-          <div key={mentor.id} className={style.divCard}>
-            <img src={mentor.image} alt='image' className={style.image} />
-            <div className='bg-slate-400 w-52'>
-              <p className='text-center pl-3 font-bold'>{mentor.name}</p>
-              <span className='flex m-auto justify-center'>
-                {mentor.rating}
-              </span>
-              <span className='flex m-auto justify-center'>
-                {mentor.language};
-                {/* {console.log(mentor.language.map((lan) => lan.includes(Language.filter(lan))))} */}
-              </span>
-            </div>
-          </div>
-        );
-      })}
+    <div className='cardMentor'>
+      <img src={img} alt={nombre} />
+      <div className='CardInfo'>
+        <p className={'cardName'}>{nombre}</p>
+        <p className={'cardCargo'}>{cargo}</p>
+      </div>
     </div>
   );
-};
+}
 
-export default CardMentors;
+export default function CardMentors({ ArrayMentors }: PropsCardsMentors) {
+  return (
+    <div className='CardsMentors'>
+      {ArrayMentors.map(({ img, cargo, nombre }: Cards) => (
+        <CardMentor
+          img={typeof img === 'string' ? img : imgPredeterminate}
+          cargo={cargo}
+          nombre={nombre}
+        />
+      ))}
+    </div>
+  );
+}
