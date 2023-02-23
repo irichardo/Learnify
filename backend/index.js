b4c00cd7f557f716ea0b3f96f635da4225a17bd2
CristianVelarde98 <cristianvelardefx@gmail.com>
2023-02-23 12:02:49 -0300

    cambios en back, implementacion del CORS

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,27 @@
-const express = require('express')
-const mongoose = require('mongoose')
-require('dotenv').config()
-const server = express()
-const routes = require('./routes/index.js')
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const server = express();
+const routes = require("./routes/index.js");
+const cors = require("cors");
 
 // variables de entorno
-const { PORT, MONGO_URL } = process.env
+const { PORT, MONGO_URL } = process.env;
 
 // middleware
-server.use(express.json())
-server.use('/', routes)
+server.use(express.json());
+server.use("/", routes);
+server.use(cors());
 
 // StricMode
-mongoose.set('strictQuery', false)
-
 mongoose.set("strictQuery", false);
+
 // coneccion con mongoDB
 mongoose
   .connect(MONGO_URL)
-  .then((res) => console.log('conectado a mongoDB atlas'))
-  .catch((err) => console.log(err))
+  .then((res) => console.log("conectado a mongoDB atlas"))
+  .catch((err) => console.log(err));
 
 server.listen(PORT, () => {
-  console.log('servidor escuchando en el puerto ' + PORT)
-})
+  console.log("servidor escuchando en el puerto " + PORT);
+});
