b4e7bf0ef7cf8a69b6f265c24569aa0db55386fc
CristianVelarde98 <cristianvelardefx@gmail.com>
2023-02-27 00:20:10 -0300

    pequeños cambios en GET de los buckets

--- backend/process/buckets/traerBucketsActivos.js
+++ backend/process/buckets/traerBucketsActivos.js
@@ -1,12 +1,10 @@
 const { conBucketsPorQuery } = require("../../controllers/buckets.controllers");
 
 const traerBucketsActivos = (type) => {
-  if (typeof type !== "string") {
-    throw new Error(`el tipo debe ser un string`);
-  }
-  if (type !== "activo" && type !== "inactivo") {
-    throw new Error(`tipo incorrecto`);
-  }
+  if (typeof type !== "string") throw new Error(`el tipo debe ser un string`);
+
+  if (type !== "activo" && type !== "inactivo")
+    throw new Error(`el type debe ser activo o inactivo`);
 
   const query = type === "activo" ? { activo: true } : { activo: false };
   return conBucketsPorQuery(query);
