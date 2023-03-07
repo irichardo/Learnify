0c98553aef832bf71e035fb175ba04e7d7c9f49d
CristianVelarde98 <cristianvelardefx@gmail.com>
2023-03-07 11:55:40 -0300

    cambios en el back, se modifico el endpoint para modificar la cantidad de tokens pero buscando por email

--- backend/process/user/modificarTokens.js
+++ backend/process/user/modificarTokens.js
@@ -2,15 +2,12 @@ const { conModificarTypeTock } = require("../../controllers/user.controller");
 const { conTraerUno } = require("../../controllers/user.controller");
 const objectId = require("mongodb").ObjectId;
 
-const modificarTokens = async (id, tokens) => {
-  const otroId = new objectId(id);
-  const query = { _id: otroId };
-
+const modificarTokens = async (email, tokens) => {
   if (typeof tokens == "string") {
     throw new Error(`el atributo tokens debe ser un numero entero`);
   }
 
-  const userTokens = (await conTraerUno(query))?.tokens;
+  const userTokens = (await conTraerUno({ email }))?.tokens;
 
   if (userTokens == undefined) {
     throw new Error(`no se encontro el usuario ${id}`);
@@ -25,10 +22,10 @@ const modificarTokens = async (id, tokens) => {
       `no tiene saldo para realizar esta operacion, la cuenta dispone de solo ${userTokens} tokens`
     );
 
-  const resp = await conModificarTypeTock(query, query2);
+  const resp = await conModificarTypeTock({ email }, query2);
 
   if (resp.modifiedCount)
-    return `el saldo anterior del usuario: ${id} era de ${
+    return `el saldo anterior del usuario: ${email} era de ${
       userTokens ? userTokens : 0
     } tokens, el saldo actual es de ${aux} tokens`;
 };
