e96f346cb4e7f35c0ad011b6ef9b4d7a3290c457
Duvanrozop <rozoparraduvan@gmail.com>
2023-03-07 08:23:57 -0500

    implement create buckets and get buckets user

--- frontend/src/pages/Profile/Profile.tsx
+++ frontend/src/pages/Profile/Profile.tsx
@@ -4,38 +4,69 @@ import default_logo from '../../assets/imgs/profile-img-default.png';
 
 import useAuthStore from '../../store/authStore';
 import { request, config } from '../../store/authStore';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import './Profile.css';
 import Loading from '../../components/Loader/Cargando';
+import { Slider } from 'antd';
+import { AxiosError } from 'axios';
 
 const Profile = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [value, setValue] = useState<number>(0);
   const [state, setState] = useState({
     img: '',
     name: '',
     email: '',
+    buckets: [],
+    tokens: 0,
   });
   const { id } = useAuthStore((state) => state);
   useEffect(() => {
     const detailUser = async () => {
       const user = await request.get(`/users?id=${id}`);
-      const { picture, name, email } = user.data;
-      console.log('💻 -> detailUser -> user.data:', user.data);
+      const { picture, name, email, buckets, tokens } = user.data;
 
       setState({
         img: picture,
         name,
         email,
+        buckets,
+        tokens,
       });
     };
 
-    id && detailUser();
+    id?.length && detailUser();
   }, [id]);
 
   if (state?.email?.length === 0) return <Loading />;
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+  };
+
+  const handleSubmitBuckets = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const nombre = inputRef.current?.value;
+
+    request
+      .post(
+        `/buckets`,
+        {
+          nombre,
+          usuario: {
+            _id: id,
+            aportes: value,
+          },
+        },
+        config
+      )
+      .then((response) => {
+        alert(response.data);
+      })
+      .catch((error) => {
+        alert(error.response.data.error);
+      });
   };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -46,10 +77,18 @@ const Profile = () => {
     });
   };
 
+  const handleSliderChange = (value: number) => {
+    setValue(value);
+  };
+
   return (
     <section className='ContianerProfile'>
       <div className='profileImg'>
         <img src={state.img} alt={state.name} />
+        <label>Nombre: {state.name}</label>
+        <label>Correo: {state.email}</label>
+        <label>Tokens: {state.tokens}</label>
+        <label>Cant. Buckets: {state.buckets?.length}</label>
       </div>
       <div className='info'>
         <form onSubmit={handleSubmit}>
@@ -67,8 +106,42 @@ const Profile = () => {
             value={state.email}
             placeholder='No se puede cambiar el correo'
           />
-          <button type='submit'>Enviar</button>
+          <button type='submit'>Actualizar Datos</button>
         </form>
+        <div className='buckets'>
+          {state.buckets.length &&
+            state.buckets.map((element) => {
+              const { bucketName, aportes } = element;
+              return (
+                <div>
+                  <h1>Nombre: {bucketName}</h1>
+                  <h1>aporte: {aportes}</h1>
+                </div>
+              );
+            })}
+        </div>
+        <section className='containerBucketsProfile'>
+          <form onSubmit={handleSubmitBuckets}>
+            <h1>Buckets</h1>
+            <label>nombre</label>
+            <input
+              ref={inputRef}
+              name='nameBucket'
+              type='text'
+              placeholder='Nombre del buckets'
+            />
+            <label>Aporte:</label>
+            <Slider
+              defaultValue={0}
+              value={value}
+              onChange={handleSliderChange}
+              tooltip={{ open: true }}
+              min={0}
+              max={state.tokens}
+            />
+            <button type='submit'>Peticion para nuevo bucket</button>
+          </form>
+        </section>
       </div>
     </section>
   );
