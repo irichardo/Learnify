8e41f4aadd537e91abba673a54c99d1d243b94b4
Duvanrozop <rozoparraduvan@gmail.com>
2023-03-08 01:43:10 -0500

    upgrade in the components for buckets

--- frontend/src/pages/Buckets/Buckets.tsx
+++ frontend/src/pages/Buckets/Buckets.tsx
@@ -7,77 +7,69 @@ import java from '../../assets/icons/iconPruebaJava.png';
 import Go from '../../assets/icons/iconPruebaGo.png';
 import arrow from '../../assets/icons/iconArrow.png';
 import luna from '../../assets/imgs/luna.png';
+import predeterminate from '../../assets/imgsPrueba/predeterminateBucket.webp';
 
 // ^ Hooks
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import stateBucket from '../../store/Buckets';
 
-type Bucket = {
-  img: string;
-  nombre: string;
-  temas: string[];
-  points: number;
-  price: number;
+// * ATN
+import { Tooltip } from 'antd';
+
+type Usuario = {
+  aportes: number;
+  _id: string;
 };
 
-const simulatorApi = [
-  {
-    img: python,
-    nombre: 'Java',
-    temas: ['spring', 'api rest', 'crud basic'],
-    points: 120,
-    price: 20,
-  },
-  {
-    img: java,
-    nombre: 'Go',
-    temas: ['fiber', 'api rest', 'crud basic'],
-    points: 120,
-    price: 20,
-  },
-  {
-    img: Go,
-    nombre: 'Python',
-    temas: ['big data', 'analitysc data'],
-    points: 120,
-    price: 20,
-  },
-];
+interface Bucket {
+  activo: boolean;
+  nombre: string;
+  tokensActuales: number;
+  usuarios: Usuario[];
+  __v: number;
+  _id: string;
+}
 
 export default function Buckets() {
-  const [option, setOption] = useState('Points');
-  const [position, setPosition] = useState(simulatorApi);
+  const { listBuckets, topBuckets, bucketsActive, initialGetBuckets } =
+    stateBucket((state) => state);
 
-  const hanldeOptions = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setOption(event.currentTarget.value);
-  };
+  // const hanldeOptions = (event: React.MouseEvent<HTMLButtonElement>) => {
+  //   setOption(event.currentTarget.value);
+  // };
+
+  useEffect(() => {
+    initialGetBuckets();
+  }, []);
 
   return (
     <section className='containerBuckets'>
       <img id='luna' src={luna} alt='luna' />
       <section className='containerPosition'>
-        {simulatorApi.map((element: Bucket, index: number) => (
-          <div key={index} className={`position${index + 1}`}>
-            <img src={element.img} alt={element.nombre} />
-          </div>
+        {topBuckets.map((element: Bucket, index: number) => (
+          <Tooltip
+            placement='topLeft'
+            title={`Nombre: ${element.nombre} - Aportes: ${element.tokensActuales} Tokens`}
+          >
+            <div key={index} className={`position${index + 1}`}>
+              <img src={predeterminate} alt={element.nombre} />
+            </div>
+          </Tooltip>
         ))}
       </section>
       <section className='containerStats'>
-        <section className='table'>
+        <section className='tableBuckets'>
           <nav>
-            <button id='Points' value='Points' onClick={hanldeOptions}>
-              Points
-            </button>
-            <button id='Price' value='Price' onClick={hanldeOptions}>
-              Price
-            </button>
+            <h1 id='Points'>Points</h1>
           </nav>
-          <div className='containerDetail'>
-            {simulatorApi.map((element: Bucket, index: number) => (
+          <div className='containerDetail2'>
+            {bucketsActive.map((element: Bucket, index: number) => (
               <div className='cardBucket' key={index}>
                 <div className='cardImg'>
-                  <img src={element.img} alt={element.nombre} />
+                  <img src={predeterminate} alt={element.nombre} />
                 </div>
-                <p>{option === 'Points' ? element.points : element.price}</p>
+                <p> Nombre: {element.nombre}</p>
+                <p>Tokens: {element.tokensActuales}</p>
               </div>
             ))}
           </div>
