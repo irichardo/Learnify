57c4d199e1e77aad501ca6acff000f74250294f6
iRichard <zerorichardx@gmail.com>
2023-03-09 08:16:17 -0500

    Again

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -6,7 +6,7 @@ import { Preview, UserApi } from '../helpers/Types/Cards';
 
 // * config axios
 const request = axios.create({
-  baseURL: 'https://learnify-70tvp9b9q-irichardo.vercel.app',
+  baseURL: 'https://learnify-git-development-irichardo.vercel.app',
   withCredentials: true,
 });
 const config = {
